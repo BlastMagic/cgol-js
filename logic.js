0c35f1f60c805332ded2075d999036fcb0dc3dfe
BlastMagic <tobewelldone@gmail.com>
2026-02-11 12:36:48 -1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = '#0000FF'; 
+ctx.fillStyle = '#FFFF00'; 
 ctx.fillRect(20, 20, 150, 100); 
